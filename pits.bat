8dcfe0e49ce8ea355933060109e1e6df072e0c9e
Yukio KANEDA <m5knt@inetsbb.com>
2018-12-14 18:20:12 +0900

    add help

--- pits.bat
+++ pits.bat
@@ -10,9 +10,19 @@ if "%1" == "clean" goto :CLEAN
 if "%1" == "build" goto :BUILD
 if "%1" == "ide" goto :IDE
 if "%1" == "idesu" goto :IDESU
+if "%1" == "help" goto :HELP
 if "%1" == "" goto :BUILD
 echo.Illigal Command "%1"
 exit /b 1
+
+:HELP
+echo.[usage] ./pits [clean^|build^|ide^|idesu^|help]
+echo.help  ... this message
+echo.clean ... clean cmake out
+echo.build ... cmake
+echo.ide   ... launch ide
+echo.idesu ... launch ide (runas admin)
+exit /b
 
 :CLEAN
 rmdir /Q /S build >nul 2>&1
