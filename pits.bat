1ee20a514efa9aa9892d0fcf646d87aff46502ac
Yukio KANEDA <m5knt@inetsbb.com>
2018-12-14 17:50:17 +0900

    update

--- pits.bat
+++ pits.bat
@@ -9,9 +9,14 @@ exit /b
 if "%1" == "clean" goto :CLEAN
 if "%1" == "build" goto :BUILD
 if "%1" == "ide" goto :IDE
+if "%1" == "admin" goto :ADMIN
 if "%1" == "" goto :BUILD
 echo.Illigal Command "%1"
 exit /b 1
+
+:CLEAN
+rmdir /Q /S build >nul 2>&1
+exit /b
 
 :BUILD
 if not exist build mkdir build
@@ -20,10 +25,10 @@ cmake -G "Visual Studio 15 2017 Win64" ..
 exit /b
 
 :IDE
-::start build/Pits.sln
-powershell start-process cmd -ArgumentList '/c start /b %CD%/build/Pits.sln""' -verb runas
+start /b build/Pits.sln
 exit /b
 
-:CLEAN
-rmdir /Q /S build >nul 2>&1
+:ADMIN
+::start build/Pits.sln
+powershell start-process cmd -ArgumentList '/c start /b %CD%/build/Pits.sln""' -verb runas
 exit /b
