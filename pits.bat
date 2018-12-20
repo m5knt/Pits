295029bffc3488570d7cc7361315c7406c13c71a
Yukio KANEDA <m5knt@inetsbb.com>
2018-12-20 16:14:57 +0900

    change command, ide to vs

--- pits.bat
+++ pits.bat
@@ -8,8 +8,8 @@ exit /b
 :MAIN
 if "%1" == "clean" goto :CLEAN
 if "%1" == "build" goto :BUILD
-if "%1" == "ide" goto :IDE
-if "%1" == "idesu" goto :IDESU
+if "%1" == "vs" goto :VS
+if "%1" == "vssu" goto :VSSU
 if "%1" == "help" goto :HELP
 if "%1" == "" goto :BUILD
 echo.Illigal Command "%1"
@@ -20,8 +20,8 @@ echo.[usage] ./pits [clean^|build^|ide^|idesu^|help]
 echo.help  ... this message
 echo.clean ... clean cmake out
 echo.build ... cmake
-echo.ide   ... launch ide
-echo.idesu ... launch ide (runas admin)
+echo.vs    ... launch vs
+echo.vssu  ... launch vs (runas admin)
 exit /b
 
 :CLEAN
@@ -34,10 +34,10 @@ cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
 exit /b
 
-:IDE
+:VS
 start /b build/Pits.sln
 exit /b
 
-:IDESU
+:VSSU
 powershell start-process cmd -ArgumentList '/c start /b %CD%/build/Pits.sln""' -verb runas
 exit /b
