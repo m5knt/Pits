e615e76ba67e38dc22d0e81eae0ea2d80fea9157
Yukio KANEDA <m5knt@inetsbb.com>
2019-01-08 19:03:59 +0900

    add string literal utils

---
+++ test/TestStringLiteral.cpp
@@ -0,0 +1,27 @@
+﻿#include "../src/StringLiteral.hpp"
+#include <uchar.h>
+#include <cassert>
+#include <string_view>
+#include <iostream>
+#include <codecvt>
+
+using namespace std::literals;
+
+int main() {
+
+#ifdef _MSC_VER
+    static_assert(Pits::StringLiteral::EncodingDetect("漢字") == Pits::StringLiteral::SJIS);
+    static_assert(Pits::StringLiteral::EncodingDetect(L"漢字") == Pits::StringLiteral::UTF16);
+#endif
+
+    static_assert(Pits::StringLiteral::EncodingDetect(u"漢字") == Pits::StringLiteral::UTF16);
+    static_assert(Pits::StringLiteral::EncodingDetect(U"漢字") == Pits::StringLiteral::UTF32);
+
+    // ビルド環境で変わるのでここでは識別出来たかだけ確認
+    static_assert(Pits::StringLiteral::EncodingDetect("漢字") != Pits::StringLiteral::OTHER);
+
+    // ビルド環境で変わるのでここでは識別出来たかだけ確認
+    static_assert(Pits::StringLiteral::EncodingDetect(L"漢字") != Pits::StringLiteral::OTHER);
+
+    return EXIT_SUCCESS;
+}
