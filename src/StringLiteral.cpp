a568487b06b85646e5be0d4d4502cb333d9c4af8
Yukio KANEDA <m5knt@inetsbb.com>
2019-01-09 14:38:50 +0900

    reserve

---
+++ src/StringLiteral.cpp
@@ -0,0 +1,24 @@
+﻿
+/**
+ * @brief プログラムソース中の文字リテラルのエンコーディングを調べるユーティリティー
+ * @author Yuki Kaneda
+ * @file 
+ */
+
+#include "StringLiteral.hpp"
+
+/*
+ *
+ */
+
+namespace Pits {
+
+/*
+ *
+ */
+
+} // namespace Pits
+
+/*
+ *
+ */
